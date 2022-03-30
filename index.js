260cfcfa092eda89b64876caf7ea1d910e62783e
Sergey Lunev <lunev.s@gmail.com>
2022-03-30 22:50:14 +0300

    ready notes

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const Note = require('./models/note')
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
 
 app.use(express.json())
 
-let notes = []
+app.use(requestLogger)
+
+app.use(cors())
+
+//app.use(express.static('build'))
+//let notes = []
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -16,18 +30,45 @@ app.get('/api/notes', (request, response) => {
         response.json(notes)
     })
 })
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note
         .findById(request.params.id)
         .then(note => {
-            response.json(note)
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
         })
+        .catch(error => next(error))
+
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(el => el.id !== id);
-    response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note
+        .findByIdAndRemove(request.params.id)
+        .then(result => {
+            //notes = notes.filter(el => el.id !== Number(request.params.id));
+            response.status(204).end();
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body;
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note
+        .findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+
 })
 
 app.post('/api/notes', (request, response) => {
@@ -50,7 +91,24 @@ app.post('/api/notes', (request, response) => {
 })
 
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 
 
