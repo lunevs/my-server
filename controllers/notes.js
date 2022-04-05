7480e9440afda8452650da9bece86f88010f676b
Sergey Lunev <lunev.s@gmail.com>
2022-04-05 20:07:02 +0300

    refactoring to async / await

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,10 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+notesRouter.get('/', async (request, response) => {
+    const notes = await Note.find({})
+    response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
@@ -19,7 +18,7 @@ notesRouter.get('/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
     const body = request.body
 
     const note = new Note({
@@ -28,19 +27,21 @@ notesRouter.post('/', (request, response, next) => {
         date: new Date()
     })
 
-    note.save()
-        .then(savedNote => {
-            response.json(savedNote)
-        })
-        .catch(error => next(error))
+    try {
+        const savedNote = await note.save()
+        response.status(201).json(savedNote)
+    } catch (exception) {
+        next(exception)
+    }
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+    try {
+        await Note.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch(exception) {
+        next(exception)
+    }
 })
 
 notesRouter.put('/:id', (request, response, next) => {
