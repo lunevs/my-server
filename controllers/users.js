c39fb6a809ac5f1a6e9c648e46f6873122e79af7
Sergey Lunev <lunev.s@gmail.com>
2022-05-04 12:28:17 +0300

    add auth

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,23 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    //const users = await User
-   ///     .find({}).populate('notes', {content: 1, date: 1})
-    //response.json(users)
     User.find({}).then(users => {
         response.json(users)
     })
 })
+
+usersRouter.get('/:id', async (request, response) => {
+    User.findById(request.params.id)
+        .then(user => {
+            if (user) {
+                response.json(user)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
 
 usersRouter.post('/', async (request, response) => {
     const requestUser = request.body
